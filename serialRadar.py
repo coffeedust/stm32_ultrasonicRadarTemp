0d8b3a26f2640f782e7ba19d6da83f4327ec005e
coffeedust <coffeedust@gmail.com>
2025-02-10 10:41:02 +0900

    complete code

--- serialRadar.py
+++ serialRadar.py
@@ -8,7 +8,11 @@ HEIGHT = 480
 angle = 0
 direction = 0
 sendingAngle = 0
+# 물체를 저장할 리스트 생성 [[0,10],[10,20],[각도,거리]...]
 objects = [[x,0] for x in range(0, 190, 10)]
+# 예시
+objects[4][1] = 200
+objects[10][1] = 150
 
 ser = serial.Serial("COM3", 115200)
 window = tk.Tk()
@@ -16,6 +20,12 @@ window.title("초음파 레이더")
 window.geometry("640x480+3000+100")
 canvas = tk.Canvas(window, width=WIDTH, height=HEIGHT, bg="black")
 canvas.pack()
+
+def drawObject(angle, distance):
+    radius = WIDTH / 2
+    x = radius + math.cos(angle * math.pi / 180) * distance
+    y = radius - math.sin(angle * math.pi / 180) * distance
+    canvas.create_oval(x-5, y-5, x+5, y+5, fill='green')
 
 def update_scan():
     global angle
@@ -28,6 +38,15 @@ def update_scan():
         sendingAngle = angle
         ser.write(f"A{angle:03d}\n".encode())
     # 거리 수신
+    if ser.in_waiting > 0:
+        data = ser.readline().decode()
+        if data[0] == 'D':
+            # 두번째 문자부터 숫자로 파싱
+            receiveDistance = int(data[1:])
+            # 해당 각도의 거리를 업데이트
+            for obj in objects:
+                if(obj[0] == sendingAngle):
+                    obj[1] = receiveDistance
     # 레이더 선 그리기
     canvas.delete("all")
     radius = WIDTH / 2
@@ -35,6 +54,9 @@ def update_scan():
     y = radius - (math.sin(angle * math.pi / 180) * radius)
     canvas.create_line(x, y, radius, radius, fill='green', width=3)
     # 감지 물체 그리기
+    for obj in objects:
+        if obj[1] != 0:
+            drawObject(obj[0], obj[1])
     # 각도 업데이트
     if direction == 0:
         angle += 1
